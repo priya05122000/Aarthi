929d9a03511ca9d2e13123f8a4b7ede76ace3703
priya05122000 <108515299+priya05122000@users.noreply.github.com>
2022-07-01 13:06:16 +0530

    Add files via upload

---
+++ day-1-exercise-3-input.py
@@ -0,0 +1,5 @@
+# Day 1/100 - Exercise 3 - Input
+# Write a program that prints the number of characters in someone's name.
+# Warning: Your name should work for different inputs, e.g. any name that you input.
+
+print("Your name is " + str(len(input("Hello, what is your name? "))) + " characters long.")
